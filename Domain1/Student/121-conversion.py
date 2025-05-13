79ee27cce7985e6ec33ba66eca1841e98224e41f
D. Sukhdeo <144715223+DeveshSukhdeo@users.noreply.github.com>
2025-05-13 09:26:22 -0400

    Update 121-conversion.py

--- Domain1/Student/121-conversion.py
+++ Domain1/Student/121-conversion.py
@@ -2,3 +2,10 @@
 rating = (input("Enter a rating between 1 and 5"))
 points = (rating) * 100
 print("You have " + (points) + " to start.")
+
+
+#new code
+# rating is supposed to be a whole number
+rating = float(input("Enter a rating between 1 and 5"))
+points = int(rating) * 100
+print("You have " + str(points) + " to start.")
