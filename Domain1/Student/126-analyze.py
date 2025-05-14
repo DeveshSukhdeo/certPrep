2c1e2bc5b51653ab98b0798b1f7ee06eb2676b80
D. Sukhdeo <144715223+DeveshSukhdeo@users.noreply.github.com>
2025-05-14 08:56:52 -0400

    Update 126-analyze.py

--- Domain1/Student/126-analyze.py
+++ Domain1/Student/126-analyze.py
@@ -3,3 +3,14 @@ print(quote[4:9])
 
 coins = ('Platinum','Gold','Silver') #What type of data structure is this?
 #Can we change Platinum to Bronze?
+
+#New Code
+quote = "The quick brown fox jumped over the lazy dog."
+print(quote[4:9])
+
+coins = ('Platinum','Gold','Silver') #What type of data structure is this?
+#Can we change Platinum to Bronze?
+coins_list = list(coins)
+coins_list[0] = "Bronze" 
+coins2 = tuple(coins_list)
+print(coins2)
