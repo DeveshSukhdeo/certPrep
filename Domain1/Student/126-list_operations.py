f6ab5bf7d44a90ced31d52894815996ad8e4cf81
D. Sukhdeo <144715223+DeveshSukhdeo@users.noreply.github.com>
2025-05-14 08:40:38 -0400

    Update 126-list_operations.py

--- Domain1/Student/126-list_operations.py
+++ Domain1/Student/126-list_operations.py
@@ -2,3 +2,9 @@ capitals=["Montgomery","Juneau","Phoenix"]
 
 
 print(capitals)
+
+#New code 
+capitals=["Montgomery","Juneau","Phoenix"]
+capitals.append("Sacramento")
+capitals.insert(-1, "Little Rock")
+print(capitals)
